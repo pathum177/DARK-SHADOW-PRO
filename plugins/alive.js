129285d0c712e1d73968df4e8f2951391c644499
ayesh406 <ayeshboys1988@gmail.com>
2025-04-20 16:24:59 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -39,7 +39,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363401249335754@newsletter',
+                    newsletterJid: '120363398661137791@newsletter',
                     newsletterName: '𝐀𝐒𝐇𝐈𝐘𝐀-MD',
                     serverMessageId: 143
                 }
