c3558bb08a086efea7747cc6cc3a3cd42d12b5ae
sulamadara1147 <madarasulaksha@gmail.com>
2025-04-22 08:55:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -117,6 +117,22 @@ mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
 }
+
+   if (config.AUTO_REACT_STATUS === "true") {
+        if (!mek.message) return;
+
+        mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
+        if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+            let emoji = ['😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '💗', '✅', '☘️', '👋', '😁', '☠️', '💩', '🧚‍♂️', '👣'];
+            let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+            await conn.readMessages([mek.key]);
+            conn.sendMessage(
+                'status@broadcast',
+                { react: { text: sigma, key: mek.key } },
+                { statusJidList: [mek.key.participant] }
+            );
+        }  
+}
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
 const content = JSON.stringify(mek.message)
