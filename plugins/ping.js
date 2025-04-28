606d7cabd8d926557a7732263cbecb2a63e89ef0
ayesh406 <ayeshboys1988@gmail.com>
2025-04-28 12:20:38 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *🔥 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> *🔥 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 SPEED : ${ping}ms 🌬️*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
