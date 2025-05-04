760a8bdcc6af18440e03ed0a3f4aee5b421d771d
ayesh406 <ayeshboys1988@gmail.com>
2025-05-04 11:54:49 +0530

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -33,7 +33,7 @@ cmd({
 `;
 
         return await conn.sendMessage(from, { 
-            image: { url: "https://files.catbox.moe/x6i10c.jpg" },
+            image: { url: "https://files.catbox.moe/08x6zl.jpg" },
             caption: repolink,contextInfo: {
                 forwardingScore: 999,
                 isForwarded: true,
