22c8f3f5552c3d41b5b5b35d105b3398d6407c8e
ayesh406 <ayeshboys1988@gmail.com>
2025-05-04 21:06:07 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃*
+> *𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃 🥷💗*
 `;
         return reply(weather);
     } catch (e) {
