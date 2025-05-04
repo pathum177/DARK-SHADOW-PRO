cf3567040d284943d4404e707402cd2a5d29fad7
ayesh406 <ayeshboys1988@gmail.com>
2025-05-04 08:19:42 +0530

    Update rw.js

--- plugins/rw.js
+++ plugins/rw.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© 🄿🄾🅆🄴🅁🄳 🅱🆈  😈 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> © 🄿🄾🅆🄴🅁🄳 🅱🆈  😈 𝐀𝐒𝐇𝐈𝐘𝐀-𝐌𝐃*;
 *`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
