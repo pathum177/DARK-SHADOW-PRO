af1244a121272c98b2358357314e63ec5cfb9cc5
pathum177 <pathummalsara177@gmail.com>
2025-05-26 15:58:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "B2tRnYJJ#koiSBJ6HTNXEupaBogg35dB-rYhVmVyc3FuzToPOxGU", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "shadow~qtBGlKiB#eZUCHOSHRfaP1KOajVwbvYTJFqChzyq_qaHCz7xPPQI", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/slm10t.jpg",
 BOT_NAME: process.env.BOT_NAME || "DARK SHADOW",
